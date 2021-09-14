4b00b44ca0177fc513a9dbe229ccc407254471fa
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-09-13 23:19:30 -0500

    Remove extra files and code

---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Items = () => <h1>Current Quantity</h1>;
+
+export default Items;
+
+// class Calculator extends React.Component {
+//   render() {
+//     return <Items quantity={ this.props.quantity }/>;
+//   }
+// }
+
+// export default Calculator;
